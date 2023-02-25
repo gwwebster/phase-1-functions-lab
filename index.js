5c195129b3d13e1644603e3d2c5129a77b259c42
George <gwwebster23@gmail.com>
2023-02-25 14:25:15 -0600

    JS functions lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42 ) {
+        return blocks - 42
+    } else if (blocks < 42 && blocks > 0) {
+        return 42 - blocks
+    } else {
+        return "please enter a number"
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264    
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+        return ((destination - start) * 264)
+    } else if (start > destination) {
+        return ((start - destination) * 264) 
+    } else {
+        return "please enter a number"
+    }
+}
+
+// let distanceInFeet = 
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
